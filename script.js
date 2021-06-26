0015705f25325b2e2eb03fed3b522371964df821
Pratik667 <44764336+Pratik667@users.noreply.github.com>
2021-06-26 15:16:47 +0530

    ds

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var myApp = angular.module("myList",[]);
 myApp.controller("myListController",function($scope){
-    // $scope.items = ["Angularjs","React JS","minsjs"];
+    $scope.items = "Workout";
     $scope.newItem = "";
     $scope.pushItem = function(){
         if($scope.newItem != ""){
